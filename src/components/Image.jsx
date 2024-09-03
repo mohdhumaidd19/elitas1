5c27223241d5b10ca73380ef2369665dc3961cbb
mohdhumaidd19 <mohdhumaidd@gmail.com>
2024-09-03 16:54:45 +0530

    1

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Image = () => {
   return (
     <div
-      className='w-[1265px] h-[450px] flex items-center justify-center overflow-hidden w-[100vw]'
+      className=' h-[450px] flex items-center justify-center overflow-hidden w-[100vw]'
       style={{
         fontFamily: 'Poppins',
         fontStyle: 'normal',
